632c7d01e3ed554a3736652c71e26b5ae4edf966
LETICIA LOPES DE LIMA SILVEIRA <11681113902@info.local>
2024-10-09 10:16:09 -0300

    .

--- contato/js/custom.js
+++ contato/js/custom.js
@@ -1,29 +1,33 @@
 (function($) {
+    $(".contactform").on("submit", function(event) {
+        event.preventDefault(); // Previne o comportamento padrão do formulário
+        $(".output_message").text("Sending..."); // Mensagem inicial enquanto envia
 
-		$(".contactform").on("submit", function() {
-			$(".output_message").text("Message Sent!");
+        var form = $(this);
+        $.ajax({
+            url: form.attr("action"),
+            method: form.attr("method"),
+            data: form.serialize(),
+            success: function(result) {
+                $(".form-inputs").css("display", "none");
+                $(".box p").css("display", "none");
+                $(".contactform").find(".output_message").removeClass("error").addClass("success");
 
-			var form = $(this);
-			$.ajax({
-				url: form.attr("action"),
-				method: form.attr("method"),
-				data: form.serialize(),
-				success: function(result) {
-					if (result == "success") {
-						$(".form-inputs").css("display", "none");
-						$(".box p").css("display", "none");
-						$(".contactform").find(".output_message").addClass("success");
-						$(".output_message").text("Message Sent!");
-					} else {
-						$(".tabs-container").css("height", "440px");
+                if (result === "success") {
+                    $(".output_message").text("Message Sent!");
+                } else {
+                    $(".tabs-container").css("height", "440px");
+                    $(".contactform").find(".output_message").removeClass("success").addClass("error");
+                    $(".output_message").text("Error Sending!");
+                }
+            },
+            error: function() {
+                $(".tabs-container").css("height", "440px");
+                $(".contactform").find(".output_message").removeClass("success").addClass("error");
+                $(".output_message").text("Error Sending!");
+            }
+        });
 
-						$(".contactform").find(".output_message").addClass("error");
-						$(".output_message").text("Error Sending!");
-					}
-				}
-			});
-
-			return false;
-		});
-
-	});
+        return false; 
+    });
+})(jQuery);
